004c412cf432c242a9253b9b2e0daa22b2c04cc0
hamzandev <hamzankechil@gmail.com>
2024-01-01 10:26:34 +0800

    create API routes

---
+++ server/routes/tasks.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => ({
+    "name": "Chocolate",
+    "type": "Ice Cream",
+    "ingredients": ["Cream", "Sugar", "Cocoa Powder"],
+    "serving_suggestion": "In a waffle cone with whipped cream and a cherry on top",
+    "calories": 250
+  }));
